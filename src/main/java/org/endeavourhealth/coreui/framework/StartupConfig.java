ef3f323d553757c4ea0e2b27b6b3c9d8094857ea
Richard Collier <ergosoftuk@gmail.com>
2018-02-05 09:40:27 +0000

    Add cleanup of JDBC drivers on shutdown

--- src/main/java/org/endeavourhealth/coreui/framework/StartupConfig.java
+++ src/main/java/org/endeavourhealth/coreui/framework/StartupConfig.java
@@ -1,5 +1,6 @@
 package org.endeavourhealth.coreui.framework;
 
+import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 import org.endeavourhealth.common.config.ConfigManager;
 import org.endeavourhealth.common.config.ConfigManagerException;
 import org.slf4j.Logger;
@@ -7,6 +8,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,6 +49,9 @@ public final class StartupConfig implements ServletContextListener {
 
     public void contextDestroyed(ServletContextEvent contextEvent) {
         LOG.trace("Shutting down...");
+
+        cleanupJDBCDrivers();
+
         for (Map.Entry<String, ContextShutdownHook> entry : shutdownHooks.entrySet()) {
             LOG.trace("\tShutting down hook [" + entry.getKey() + "]");
             entry.getValue().contextShutdown();
@@ -51,5 +59,36 @@ public final class StartupConfig implements ServletContextListener {
         LOG.trace("...Shutdown done.");
     }
 
+    private void cleanupJDBCDrivers() {
+        LOG.trace("Cleaning up JDBC drivers...");
+        try {
+            LOG.info("Calling MySQL AbandonedConnectionCleanupThread shutdown");
+            AbandonedConnectionCleanupThread.shutdown();
+
+        } catch (InterruptedException e) {
+            LOG.error("Error calling MySQL AbandonedConnectionCleanupThread shutdown {}", e);
+        }
+
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+
+            if (driver.getClass().getClassLoader() == cl) {
+
+                try {
+                    LOG.info("Deregistering JDBC driver {}", driver);
+                    DriverManager.deregisterDriver(driver);
+
+                } catch (SQLException ex) {
+                    LOG.error("Error deregistering JDBC driver {}", driver, ex);
+                }
+
+            } else {
+                LOG.trace("Not deregistering JDBC driver {} as it does not belong to this webapp's ClassLoader", driver);
+            }
+        }
+    }
 
 }
