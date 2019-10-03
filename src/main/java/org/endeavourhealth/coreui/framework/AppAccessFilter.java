ece365b071f857a9779e9e62111de4639730bc18
stuart <stuart@bearclawsolutions.co.uk>
2019-10-03 10:08:14 +0100

    Add new servlet filter to check user has appropriate access before all API calls to the backend

---
+++ src/main/java/org/endeavourhealth/coreui/framework/AppAccessFilter.java
@@ -0,0 +1,75 @@
+package org.endeavourhealth.coreui.framework;
+
+import org.endeavourhealth.common.security.usermanagermodel.models.caching.UserCache;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppAccessFilter implements Filter {
+    private static final Logger logger = Logger.getLogger(AppAccessFilter.class.getName());
+    private static String appName = "";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        appName = filterConfig.getInitParameter("app_name");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        //check for authenticated user
+        Principal principal = httpServletRequest.getUserPrincipal();
+        if (principal != null) {
+
+            //basic servlet request info
+            String userId = httpServletRequest.getUserPrincipal().getName();
+
+            String projectId = httpServletRequest.getHeader("projectId");
+
+            String pathURI = httpServletRequest.getRequestURI();
+
+            String appId = appName;
+
+            // boolean isUserAllowedAccess = appId.equalsIgnoreCase("User Manager");
+            Boolean isUserAllowedAccess = false;
+
+            if (projectId == null && (pathURI.contains("getUserProfile") || pathURI.contains("getProjects"))) {
+                isUserAllowedAccess = true;
+            } else {
+                try {
+                    isUserAllowedAccess = UserCache.getUserProjectApplicationAccess(userId, projectId, appId);
+
+                } catch (Exception e) {
+                    logger.log(Level.INFO, "Error: " + e.getMessage());
+                    httpServletResponse.sendError(403, "Access is Forbidden");
+                }
+            }
+            if (!isUserAllowedAccess) {
+
+                httpServletResponse.sendError(403, "Access is Forbidden");
+                return;
+
+            } else {
+                chain.doFilter(request, response);
+            }
+        } else {
+            //continue valve pipeline
+            chain.doFilter(request, response);
+        }
+
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
